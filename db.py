945578929e65eef997dcee102e90d9d23645b3ec
Theo QUEZEL PERRON <explo8413@gmail.com>
2022-11-27 21:38:20 +0100

    add second version for online server

--- db.py
+++ db.py
@@ -5,6 +5,7 @@ from werkzeug.local import LocalProxy
 from pymongo import MongoClient
 
 client = MongoClient('mongodb://127.0.0.1:27017')
+# client = MongoClient('mongodb+srv://ovhUser:h0Ip6pe9NJnK1EnC@urbanisationceri.vpzkh.mongodb.net/?retryWrites=true&w=majority')
 
 
 def get_db():
